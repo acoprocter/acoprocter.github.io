c840628f5e7d85e709c6efce3e4447e805f0b210
acoprocter <acoprocter@gmail.com>
2017-02-08 17:41:53 +1300

    default layout and index page

---
+++ js/jquery.main.js
@@ -0,0 +1,533 @@
+// page init
+jQuery(function(){
+	initTouchNav();
+	initDropDownClasses();
+	jQuery('input, textarea').placeholder();
+});
+
+// handle dropdowns on mobile devices
+function initTouchNav() {
+	lib.each(lib.queryElementsBySelector('#nav'), function(){
+		new TouchNav({
+			navBlock: this
+		});
+	});
+}
+
+// add classes if item has dropdown
+function initDropDownClasses() {
+	jQuery('#nav li').each(function() {
+		var item = jQuery(this);
+		var drop = item.find('ul');
+		var link = item.find('a').eq(0);
+		if(drop.length) {
+			item.addClass('has-drop-down');
+			if(link.length) link.addClass('has-drop-down-a');
+		}
+	});
+}
+
+/*! http://mths.be/placeholder v2.0.6 by @mathias */
+;(function(window, document, $) {
+
+	var isInputSupported = 'placeholder' in document.createElement('input'),
+	    isTextareaSupported = 'placeholder' in document.createElement('textarea'),
+	    prototype = $.fn,
+	    valHooks = $.valHooks,
+	    hooks,
+	    placeholder;
+	if(navigator.userAgent.indexOf('Opera/') != -1) {
+		isInputSupported = isTextareaSupported = false;
+	}
+	if (isInputSupported && isTextareaSupported) {
+
+		placeholder = prototype.placeholder = function() {
+			return this;
+		};
+
+		placeholder.input = placeholder.textarea = true;
+
+	} else {
+
+		placeholder = prototype.placeholder = function() {
+			var $this = this;
+			$this
+				.filter((isInputSupported ? 'textarea' : ':input') + '[placeholder]')
+				.not('.placeholder')
+				.bind({
+					'focus.placeholder': clearPlaceholder,
+					'blur.placeholder': setPlaceholder
+				})
+				.data('placeholder-enabled', true)
+				.trigger('blur.placeholder');
+			return $this;
+		};
+
+		placeholder.input = isInputSupported;
+		placeholder.textarea = isTextareaSupported;
+
+		hooks = {
+			'get': function(element) {
+				var $element = $(element);
+				return $element.data('placeholder-enabled') && $element.hasClass('placeholder') ? '' : element.value;
+			},
+			'set': function(element, value) {
+				var $element = $(element);
+				if (!$element.data('placeholder-enabled')) {
+					return element.value = value;
+				}
+				if (value == '') {
+					element.value = value;
+					// Issue #56: Setting the placeholder causes problems if the element continues to have focus.
+					if (element != document.activeElement) {
+						// We can’t use `triggerHandler` here because of dummy text/password inputs :(
+						setPlaceholder.call(element);
+					}
+				} else if ($element.hasClass('placeholder')) {
+					clearPlaceholder.call(element, true, value) || (element.value = value);
+				} else {
+					element.value = value;
+				}
+				// `set` can not return `undefined`; see http://jsapi.info/jquery/1.7.1/val#L2363
+				return $element;
+			}
+		};
+
+		isInputSupported || (valHooks.input = hooks);
+		isTextareaSupported || (valHooks.textarea = hooks);
+
+		$(function() {
+			// Look for forms
+			$(document).delegate('form', 'submit.placeholder', function() {
+				// Clear the placeholder values so they don’t get submitted
+				var $inputs = $('.placeholder', this).each(clearPlaceholder);
+				setTimeout(function() {
+					$inputs.each(setPlaceholder);
+				}, 10);
+			});
+		});
+
+		// Clear placeholder values upon page reload
+		$(window).bind('beforeunload.placeholder', function() {
+			$('.placeholder').each(function() {
+				this.value = '';
+			});
+		});
+
+	}
+
+	function args(elem) {
+		// Return an object of element attributes
+		var newAttrs = {},
+		    rinlinejQuery = /^jQuery\d+$/;
+		$.each(elem.attributes, function(i, attr) {
+			if (attr.specified && !rinlinejQuery.test(attr.name)) {
+				newAttrs[attr.name] = attr.value;
+			}
+		});
+		return newAttrs;
+	}
+
+	function clearPlaceholder(event, value) {
+		var input = this,
+		    $input = $(input),
+		    hadFocus;
+		if (input.value == $input.attr('placeholder') && $input.hasClass('placeholder')) {
+			hadFocus = input == document.activeElement;
+			if ($input.data('placeholder-password')) {
+				$input = $input.hide().next().show().attr('id', $input.removeAttr('id').data('placeholder-id'));
+				// If `clearPlaceholder` was called from `$.valHooks.input.set`
+				if (event === true) {
+					return $input[0].value = value;
+				}
+				$input.focus();
+			} else {
+				input.value = '';
+				$input.removeClass('placeholder');
+			}
+			hadFocus && input.select();
+		}
+	}
+
+	function setPlaceholder() {
+		var $replacement,
+		    input = this,
+		    $input = $(input),
+		    $origInput = $input,
+		    id = this.id;
+		if (input.value == '') {
+			if (input.type == 'password') {
+				if (!$input.data('placeholder-textinput')) {
+					try {
+						$replacement = $input.clone().attr({ 'type': 'text' });
+					} catch(e) {
+						$replacement = $('<input>').attr($.extend(args(this), { 'type': 'text' }));
+					}
+					$replacement
+						.removeAttr('name')
+						.data({
+							'placeholder-password': true,
+							'placeholder-id': id
+						})
+						.bind('focus.placeholder', clearPlaceholder);
+					$input
+						.data({
+							'placeholder-textinput': $replacement,
+							'placeholder-id': id
+						})
+						.before($replacement);
+				}
+				$input = $input.removeAttr('id').hide().prev().attr('id', id).show();
+				// Note: `$input[0] != input` now!
+			}
+			$input.addClass('placeholder');
+			$input[0].value = $input.attr('placeholder');
+		} else {
+			$input.removeClass('placeholder');
+		}
+	}
+
+}(this, document, jQuery));
+
+// navigation accesibility module
+function TouchNav(opt) {
+	this.options = {
+		hoverClass: 'hover',
+		menuItems: 'li',
+		menuOpener: 'a',
+		menuDrop: 'ul',
+		navBlock: null
+	};
+	for(var p in opt) {
+		if(opt.hasOwnProperty(p)) {
+			this.options[p] = opt[p];
+		}
+	}
+	this.init();
+}
+TouchNav.isActiveOn = function(elem) {
+	return elem && elem.touchNavActive;
+};
+TouchNav.prototype = {
+	init: function() {
+		if(typeof this.options.navBlock === 'string') {
+			this.menu = document.getElementById(this.options.navBlock);
+		} else if(typeof this.options.navBlock === 'object') {
+			this.menu = this.options.navBlock;
+		}
+		if(this.menu) {
+			this.addEvents();
+		}
+	},
+	addEvents: function() {
+		// attach event handlers
+		var self = this;
+		this.menuItems = lib.queryElementsBySelector(this.options.menuItems, this.menu);
+
+		for(var i = 0; i < this.menuItems.length; i++) {
+			(function(i){
+				var item = self.menuItems[i],
+					currentDrop = lib.queryElementsBySelector(self.options.menuDrop, item)[0],
+					currentOpener = lib.queryElementsBySelector(self.options.menuOpener, item)[0];
+
+				// only for touch input devices
+				if( (self.isTouchDevice || navigator.msPointerEnabled) && currentDrop && currentOpener) {
+					lib.event.add(currentOpener, 'click', lib.bind(self.clickHandler, self));
+					lib.event.add(currentOpener, navigator.msPointerEnabled ? 'MSPointerDown' : 'touchstart', function(e){
+						if(navigator.msPointerEnabled && e.pointerType !== e.MSPOINTER_TYPE_TOUCH) return;
+						self.touchFlag = true;
+						self.currentItem = item;
+						self.currentLink = currentOpener;
+						self.pressHandler.apply(self, arguments);
+					});
+				}
+				// for desktop computers and touch devices
+				lib.event.add(item, 'mouseover', function(){
+					if(!self.touchFlag) {
+						self.currentItem = item;
+						self.mouseoverHandler();
+					}
+				});
+				lib.event.add(item, 'mouseout', function(){
+					if(!self.touchFlag) {
+						self.currentItem = item;
+						self.mouseoutHandler();
+					}
+				});
+				item.touchNavActive = true;
+			})(i);
+		}
+
+		// hide dropdowns when clicking outside navigation
+		if(this.isTouchDevice || navigator.msPointerEnabled) {
+			lib.event.add(document, navigator.msPointerEnabled ? 'MSPointerDown' : 'touchstart', lib.bind(this.clickOutsideHandler, this));
+		}
+	},
+	mouseoverHandler: function() {
+		lib.addClass(this.currentItem, this.options.hoverClass);
+		jQuery(this.currentItem).trigger('itemhover');
+	},
+	mouseoutHandler: function() {
+		lib.removeClass(this.currentItem, this.options.hoverClass);
+		jQuery(this.currentItem).trigger('itemleave');
+	},
+	hideActiveDropdown: function() {
+		for(var i = 0; i < this.menuItems.length; i++) {
+			if(lib.hasClass(this.menuItems[i], this.options.hoverClass)) {
+				lib.removeClass(this.menuItems[i], this.options.hoverClass);
+				jQuery(this.menuItems[i]).trigger('itemleave');
+			}
+		}
+		this.activeParent = null;
+	},
+	pressHandler: function(e) {
+		// hide previous drop (if active)
+		if(this.currentItem !== this.activeParent) {
+			if(this.activeParent && this.currentItem.parentNode === this.activeParent.parentNode) {
+				lib.removeClass(this.activeParent, this.options.hoverClass);
+			} else if(!this.isParent(this.activeParent, this.currentLink)) {
+				this.hideActiveDropdown();
+			}
+		}
+		// handle current drop
+		this.activeParent = this.currentItem;
+		if(lib.hasClass(this.currentItem, this.options.hoverClass)) {
+			this.preventCurrentClick = false;
+		} else {
+			e.preventDefault();
+			this.preventCurrentClick = true;
+			lib.addClass(this.currentItem, this.options.hoverClass);
+			jQuery(this.currentItem).trigger('itemhover');
+		}
+	},
+	clickHandler: function(e) {
+		// prevent first click on link
+		if(this.preventCurrentClick || typeof this.preventCurrentClick === 'undefined') {
+			e.preventDefault();
+		}
+	},
+	clickOutsideHandler: function(event) {
+		if(navigator.msPointerEnabled && event.pointerType !== event.MSPOINTER_TYPE_TOUCH) return;
+		var e = event.changedTouches ? event.changedTouches[0] : event;
+		if(this.activeParent && !this.isParent(this.menu, e.target)) {
+			this.hideActiveDropdown();
+			this.touchFlag = false;
+		}
+	},
+	isParent: function(parent, child) {
+		while(child.parentNode) {
+			if(child.parentNode == parent) {
+				return true;
+			}
+			child = child.parentNode;
+		}
+		return false;
+	},
+	isTouchDevice: (function() {
+		try {
+			return (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) || navigator.userAgent.indexOf('IEMobile') != -1;
+		} catch (e) {
+			return false;
+		}
+	}())
+};
+
+/*
+ * Utility module
+ */
+lib = {
+	hasClass: function(el,cls) {
+		return el && el.className ? el.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)')) : false;
+	},
+	addClass: function(el,cls) {
+		if (el && !this.hasClass(el,cls)) el.className += " "+cls;
+	},
+	removeClass: function(el,cls) {
+		if (el && this.hasClass(el,cls)) {el.className=el.className.replace(new RegExp('(\\s|^)'+cls+'(\\s|$)'),' ');}
+	},
+	extend: function(obj) {
+		for(var i = 1; i < arguments.length; i++) {
+			for(var p in arguments[i]) {
+				if(arguments[i].hasOwnProperty(p)) {
+					obj[p] = arguments[i][p];
+				}
+			}
+		}
+		return obj;
+	},
+	each: function(obj, callback) {
+		var property, len;
+		if(typeof obj.length === 'number') {
+			for(property = 0, len = obj.length; property < len; property++) {
+				if(callback.call(obj[property], property, obj[property]) === false) {
+					break;
+				}
+			}
+		} else {
+			for(property in obj) {
+				if(obj.hasOwnProperty(property)) {
+					if(callback.call(obj[property], property, obj[property]) === false) {
+						break;
+					}
+				}
+			}
+		}
+	},
+	event: (function() {
+		var fixEvent = function(e) {
+			e = e || window.event;
+			if(e.isFixed) return e; else e.isFixed = true;
+			if(!e.target) e.target = e.srcElement;
+			e.preventDefault = e.preventDefault || function() {this.returnValue = false;};
+			e.stopPropagation = e.stopPropagation || function() {this.cancelBubble = true;};
+			return e;
+		};
+		return {
+			add: function(elem, event, handler) {
+				if(!elem.events) {
+					elem.events = {};
+					elem.handle = function(e) {
+						var ret, handlers = elem.events[e.type];
+						e = fixEvent(e);
+						for(var i = 0, len = handlers.length; i < len; i++) {
+							if(handlers[i]) {
+								ret = handlers[i].call(elem, e);
+								if(ret === false) {
+									e.preventDefault();
+									e.stopPropagation();
+								}
+							}
+						}
+					};
+				}
+				if(!elem.events[event]) {
+					elem.events[event] = [];
+					if(elem.addEventListener) elem.addEventListener(event, elem.handle, false);
+					else if(elem.attachEvent) elem.attachEvent('on'+event, elem.handle);
+				}
+				elem.events[event].push(handler);
+			},
+			remove: function(elem, event, handler) {
+				var handlers = elem.events[event];
+				for(var i = handlers.length - 1; i >= 0; i--) {
+					if(handlers[i] === handler) {
+						handlers.splice(i,1);
+					}
+				}
+				if(!handlers.length) {
+					delete elem.events[event];
+					if(elem.removeEventListener) elem.removeEventListener(event, elem.handle, false);
+					else if(elem.detachEvent) elem.detachEvent('on'+event, elem.handle);
+				}
+			}
+		};
+	}()),
+	queryElementsBySelector: function(selector, scope) {
+		scope = scope || document;
+		if(!selector) return [];
+		if(selector === '>*') return scope.children;
+		if(typeof document.querySelectorAll === 'function') {
+			return scope.querySelectorAll(selector);
+		}
+		var selectors = selector.split(',');
+		var resultList = [];
+		for(var s = 0; s < selectors.length; s++) {
+			var currentContext = [scope || document];
+			var tokens = selectors[s].replace(/^\s+/,'').replace(/\s+$/,'').split(' ');
+			for (var i = 0; i < tokens.length; i++) {
+				token = tokens[i].replace(/^\s+/,'').replace(/\s+$/,'');
+				if (token.indexOf('#') > -1) {
+					var bits = token.split('#'), tagName = bits[0], id = bits[1];
+					var element = document.getElementById(id);
+					if (element && tagName && element.nodeName.toLowerCase() != tagName) {
+						return [];
+					}
+					currentContext = element ? [element] : [];
+					continue;
+				}
+				if (token.indexOf('.') > -1) {
+					var bits = token.split('.'), tagName = bits[0] || '*', className = bits[1], found = [], foundCount = 0;
+					for (var h = 0; h < currentContext.length; h++) {
+						var elements;
+						if (tagName == '*') {
+							elements = currentContext[h].getElementsByTagName('*');
+						} else {
+							elements = currentContext[h].getElementsByTagName(tagName);
+						}
+						for (var j = 0; j < elements.length; j++) {
+							found[foundCount++] = elements[j];
+						}
+					}
+					currentContext = [];
+					var currentContextIndex = 0;
+					for (var k = 0; k < found.length; k++) {
+						if (found[k].className && found[k].className.match(new RegExp('(\\s|^)'+className+'(\\s|$)'))) {
+							currentContext[currentContextIndex++] = found[k];
+						}
+					}
+					continue;
+				}
+				if (token.match(/^(\w*)\[(\w+)([=~\|\^\$\*]?)=?"?([^\]"]*)"?\]$/)) {
+					var tagName = RegExp.$1 || '*', attrName = RegExp.$2, attrOperator = RegExp.$3, attrValue = RegExp.$4;
+					if(attrName.toLowerCase() == 'for' && this.browser.msie && this.browser.version < 8) {
+						attrName = 'htmlFor';
+					}
+					var found = [], foundCount = 0;
+					for (var h = 0; h < currentContext.length; h++) {
+						var elements;
+						if (tagName == '*') {
+							elements = currentContext[h].getElementsByTagName('*');
+						} else {
+							elements = currentContext[h].getElementsByTagName(tagName);
+						}
+						for (var j = 0; elements[j]; j++) {
+							found[foundCount++] = elements[j];
+						}
+					}
+					currentContext = [];
+					var currentContextIndex = 0, checkFunction;
+					switch (attrOperator) {
+						case '=': checkFunction = function(e) { return (e.getAttribute(attrName) == attrValue) }; break;
+						case '~': checkFunction = function(e) { return (e.getAttribute(attrName).match(new RegExp('(\\s|^)'+attrValue+'(\\s|$)'))) }; break;
+						case '|': checkFunction = function(e) { return (e.getAttribute(attrName).match(new RegExp('^'+attrValue+'-?'))) }; break;
+						case '^': checkFunction = function(e) { return (e.getAttribute(attrName).indexOf(attrValue) == 0) }; break;
+						case '$': checkFunction = function(e) { return (e.getAttribute(attrName).lastIndexOf(attrValue) == e.getAttribute(attrName).length - attrValue.length) }; break;
+						case '*': checkFunction = function(e) { return (e.getAttribute(attrName).indexOf(attrValue) > -1) }; break;
+						default : checkFunction = function(e) { return e.getAttribute(attrName) };
+					}
+					currentContext = [];
+					var currentContextIndex = 0;
+					for (var k = 0; k < found.length; k++) {
+						if (checkFunction(found[k])) {
+							currentContext[currentContextIndex++] = found[k];
+						}
+					}
+					continue;
+				}
+				tagName = token;
+				var found = [], foundCount = 0;
+				for (var h = 0; h < currentContext.length; h++) {
+					var elements = currentContext[h].getElementsByTagName(tagName);
+					for (var j = 0; j < elements.length; j++) {
+						found[foundCount++] = elements[j];
+					}
+				}
+				currentContext = found;
+			}
+			resultList = [].concat(resultList,currentContext);
+		}
+		return resultList;
+	},
+	trim: function (str) {
+		return str.replace(/^\s+/, '').replace(/\s+$/, '');
+	},
+	bind: function(f, scope, forceArgs){
+		return function() {return f.apply(scope, typeof forceArgs !== 'undefined' ? [forceArgs] : arguments);};
+	}
+};
+
+/*! matchMedia() polyfill - Test a CSS media type/query in JS. Authors & copyright (c) 2012: Scott Jehl, Paul Irish, Nicholas Zakas. Dual MIT/BSD license */
+;window.matchMedia=window.matchMedia||(function(e,f){var c,a=e.documentElement,b=a.firstElementChild||a.firstChild,d=e.createElement("body"),g=e.createElement("div");g.id="mq-test-1";g.style.cssText="position:absolute;top:-100em";d.appendChild(g);return function(h){g.innerHTML='&shy;<style media="'+h+'"> #mq-test-1 { width: 42px; }</style>';a.insertBefore(d,b);c=g.offsetWidth==42;a.removeChild(d);return{matches:c,media:h}}})(document);
+
+/*! Picturefill - Responsive Images that work today. (and mimic the proposed Picture element with divs). Author: Scott Jehl, Filament Group, 2012 | License: MIT/GPLv2 */
+;(function(a){"use strict";a.picturefill=function(){for(var b=a.document.getElementsByTagName("div"),c=0,d=b.length;d>c;c++)if(null!==b[c].getAttribute("data-picture")){for(var e=b[c].getElementsByTagName("div"),f=[],g=0,h=e.length;h>g;g++){var i=e[g].getAttribute("data-media");(!i||a.matchMedia&&a.matchMedia(i).matches)&&f.push(e[g])}var j=b[c].getElementsByTagName("img")[0];f.length?(j||(j=a.document.createElement("img"),j.alt=b[c].getAttribute("data-alt"),b[c].appendChild(j)),j.src=f.pop().getAttribute("data-src")):j&&b[c].removeChild(j)}},a.addEventListener?(a.addEventListener("resize",a.picturefill,!1),a.addEventListener("DOMContentLoaded",function(){a.picturefill(),a.removeEventListener("load",a.picturefill,!1)},!1),a.addEventListener("load",a.picturefill,!1)):a.attachEvent&&a.attachEvent("onload",a.picturefill)})(this);
